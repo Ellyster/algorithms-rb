6c0447b76193e6673db8eb6239bf22547d71161a
Alonso J. Gragera Aguaza <alonso@is.s.u-tokyo.ac.jp>
2015-04-02 09:31:46 +0900

    Added unit testing with RSpec

---
+++ spec/sort_spec.rb
@@ -0,0 +1,32 @@
+$: << File.join(File.expand_path(File.dirname(__FILE__)), '..', 'lib')
+
+require 'algorithms-rb'
+
+include Algorithms
+include Sequences
+
+describe "Sorting algorithms for sequences" do
+
+  before(:each) do
+    @algorithms = %w(bubble_sort insertion_sort selection_sort merge_sort shell_sort)
+  end
+
+  it "should work with empty arrays" do
+    @algorithms.each { |name| expect(Sort.send(name, [])).to eq([]) }
+  end
+
+  it "should work for an array of size 1" do
+    @algorithms.each { |name| expect(Sort.send(name, [1])).to eq([1]) }
+  end
+
+  it "should work for arrays of random numbers" do
+    n = 250
+    array = Array(-n..n) { rand(n) }
+    @algorithms.each { |name| expect(Sort.send(name, array.dup)).to eq(array.sort) }
+  end
+
+  it "should work for arrays of repeated numbers" do
+    array = [3,3,3,2,2,2,1,1,1]
+    @algorithms.each { |name| expect(Sort.send(name, array.dup)).to eq(array.sort) }
+  end
+end
